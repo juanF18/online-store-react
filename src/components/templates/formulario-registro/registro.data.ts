989d3fbbf271727301087bcff805251a26dcf7fc
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-06-26 10:52:56 -0500

    se modifican unas validaciones

--- src/components/templates/formulario-registro/registro.data.ts
+++ src/components/templates/formulario-registro/registro.data.ts
@@ -11,12 +11,14 @@ export function valoresIniciales() {
 
 export function validationSchema() {
   return Yup.object().shape({
-    nombre: Yup.string().required("El nombre es requerido"),
+    nombre: Yup.string()
+      .required("El nombre es requerido")
+      .min(3, "El nombre es de minimo 3 caracteres"),
     email: Yup.string()
       .email("Correo electrónico inválido")
       .required("El correo es requerido"),
     contrasena: Yup.string()
-      .min(6, "La contraseña debe tener al menos 6 caracteres")
+      .min(8, "La contraseña debe tener al menos 6 caracteres")
       .required("La contraseña es requerida"),
     confirmarContrasena: Yup.string()
       .oneOf(
