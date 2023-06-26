40c487b974d18ef5ecd30dc03eceb6935c2620a6
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-06-26 11:49:09 -0500

    Se modifica estilos de boton y formulario registro

--- src/components/atoms/boton.tsx
+++ src/components/atoms/boton.tsx
@@ -5,7 +5,7 @@ export function Boton({ label }: BotonProps) {
   return (
     <button
       type="submit"
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
     >
       {label}
     </button>
